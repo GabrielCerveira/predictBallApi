d73f703ff04b6c0da2d8eb404d4e1e21ba36352f
GabrielCerveira <gabrieloliveira033@outlook.com>
2023-01-10 18:28:29 -0300

    espacamento no server

--- src/server.js
+++ src/server.js
@@ -10,14 +10,12 @@ app.use(express.json())
 //Routes of API
 app.use(routes)
 
-
 //Conecta MongoDB
-
 mongoose
     .connect("mongodb://localhost/predictball")
     .then(() => {
         console.log("Conectamos ao MongoDB!")
-        app.listen(4000,()=>{
+        app.listen(4000,() => {
             console.log("server is listing")
         })
         mongoose.Promise = global.Promise
